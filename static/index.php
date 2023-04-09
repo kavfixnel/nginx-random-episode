e749c45910ea38741a7bd88861cf0ac5f448cd1e
Alexander Proschek <alexander.proschek@protonmail.com>
2023-04-09 16:26:18 -0700

    4 dynamic front front page (#8)
    * feat: Generate front page with php and add metadata info to shows

---
+++ static/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Random Episode</title>
+    <link rel="stylesheet" type="text/css" href="index.css" /> 
+  </head>
+  <body>
+    <?php
+      $services = array_diff(scandir('/var/episodes/'), array('..', '.'));
+      foreach($services as $service) {
+        echo "<div class=\"service\">";
+          echo "<h2>Random on $service</h2>";
+          echo "<div class=\"show-list\">";
+          $shows = array_diff(scandir("/var/episodes/$service/"), array('..', '.'));
+          foreach($shows as $show) {  
+            $jsonString=file_get_contents("/var/episodes/$service/$show/metadata.json");
+            $jsonData=json_decode($jsonString,true);
+
+            $slug = $jsonData["slug"];
+            $imgref = $jsonData["imgref"];
+            
+            echo "<a class=\"show\" href=\"/random/$service/$show\">";
+            echo "<img src=\"$imgref\" alt=\"$slug Show Poster\" class=\"show-image\">";
+            echo "<p class=\"show-title\">$slug</p>";
+            echo "</a>";
+          }
+          echo "</div>";
+        echo "</div>";
+      }
+    ?>
+  </body>
+</html>
