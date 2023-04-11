ddd5872e6d6f4045d1cfa2e695de4d03b0d0583b
Alexander Proschek <alexander.proschek@protonmail.com>
2023-04-10 22:00:05 -0700

    feat: UI for season based randomization

--- static/index.php
+++ static/index.php
@@ -10,24 +10,36 @@
     <?php
       $services = array_diff(scandir('/var/episodes/'), array('..', '.'));
       foreach($services as $service) {
-        echo "<div class=\"service\">";
+        echo '<div class="service">';
           echo "<h2>Random on $service</h2>";
-          echo "<div class=\"show-list\">";
+          echo '<div class="show-list">';
           $shows = array_diff(scandir("/var/episodes/$service/"), array('..', '.'));
-          foreach($shows as $show) {  
+          foreach($shows as $show) {
             $jsonString=file_get_contents("/var/episodes/$service/$show/metadata.json");
             $jsonData=json_decode($jsonString,true);
 
             $slug = $jsonData["slug"];
             $imgRef = $jsonData["imgRef"];
+            $numSeasons = $jsonData["numSeasons"];
             
-            echo "<a class=\"show\" href=\"/random/$service/$show\">";
-            echo "<img src=\"$imgRef\" alt=\"$slug Show Poster\" class=\"show-image\">";
-            echo "<p class=\"show-title\">$slug</p>";
-            echo "</a>";
+            echo '<div class="show">';
+              echo "<a class=\"show-all\" href=\"/random/$service/$show\">";
+                echo "<img src=\"$imgRef\" alt=\"$slug Show Poster\" class=\"show-image\">";
+                echo "<p class=\"show-title\">$slug</p>";
+              echo '</a>';
+              echo "<input type=\"checkbox\" id=\"switch-$show\" class=\"toggle\">";
+              echo "<label class=\"lbl-toggle\" for=\"switch-$show\">By Seasons</label>";
+              echo '<ul class="season-list collapsible-content">';
+              for ($season = 1; $season <= $numSeasons; $season++) {
+                echo '<li class="season">';
+                  echo "<a href=\"/random/$service/$show/s$season\">Season $season</a>";
+                echo '</li>';
+              }
+              echo '</input>';
+            echo '</div>';
           }
-          echo "</div>";
-        echo "</div>";
+          echo '</div>';
+        echo '</div>';
       }
     ?>
   </body>
